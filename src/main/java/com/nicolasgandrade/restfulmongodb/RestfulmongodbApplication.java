575483048c441b586f28afaaca84f458fcf98042
Nicolas Guerrero <nicolasguerrero72@gmail.com>
2021-12-03 18:56:27 -0300

    first commit

---
+++ src/main/java/com/nicolasgandrade/restfulmongodb/RestfulmongodbApplication.java
@@ -0,0 +1,13 @@
+package com.nicolasgandrade.restfulmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulmongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulmongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nicolasgandrade/restfulmongodb/RestfulmongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicolasgandrade.restfulmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulmongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
