03854d4afca5fd7b1a31222e0175cea202b1dcd5
nicolasgandrade <nicolasguerrero72@gmail.com>
2021-12-13 19:25:45 -0300

    feat: find users by id in url; handle "not found" exceptions

--- src/main/java/com/nicolasgandrade/restfulmongodb/services/UserService.java
+++ src/main/java/com/nicolasgandrade/restfulmongodb/services/UserService.java
@@ -2,10 +2,12 @@ package com.nicolasgandrade.restfulmongodb.services;
 
 import com.nicolasgandrade.restfulmongodb.domain.User;
 import com.nicolasgandrade.restfulmongodb.repository.UserRepository;
+import com.nicolasgandrade.restfulmongodb.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -15,5 +17,10 @@ public class UserService {
 
     public List<User> findAll() {
         return repo.findAll();
+    }
+
+    public User findById(String id) {
+        Optional<User> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
     }
 }
