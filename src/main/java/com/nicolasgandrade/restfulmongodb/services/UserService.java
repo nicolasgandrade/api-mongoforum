c44b7a944e3d8e65cfd9d4aea836ebfe7b2aa651
nicolasgandrade <nicolasguerrero72@gmail.com>
2021-12-13 19:39:14 -0300

    feat: feat: insert User with method post

--- src/main/java/com/nicolasgandrade/restfulmongodb/services/UserService.java
+++ src/main/java/com/nicolasgandrade/restfulmongodb/services/UserService.java
@@ -1,6 +1,7 @@
 package com.nicolasgandrade.restfulmongodb.services;
 
 import com.nicolasgandrade.restfulmongodb.domain.User;
+import com.nicolasgandrade.restfulmongodb.dto.UserDTO;
 import com.nicolasgandrade.restfulmongodb.repository.UserRepository;
 import com.nicolasgandrade.restfulmongodb.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,5 +23,14 @@ public class UserService {
     public User findById(String id) {
         Optional<User> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+    }
+
+    public User insert(User obj) {
+        return repo.insert(obj);
+    }
+
+    //Retorna um user a partir do dto
+    public User fromDto(UserDTO objDto) {
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
 }
