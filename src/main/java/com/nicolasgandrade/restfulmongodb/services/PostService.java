09f749bf3d80d274c40a9a29a1fb3eed4ee5b1e5
nicolasgandrade <nicolasguerrero72@gmail.com>
2021-12-14 16:24:02 -0300

    feat: add search by title functionality using spring data

--- src/main/java/com/nicolasgandrade/restfulmongodb/services/PostService.java
+++ src/main/java/com/nicolasgandrade/restfulmongodb/services/PostService.java
@@ -6,6 +6,7 @@ import com.nicolasgandrade.restfulmongodb.services.exception.ObjectNotFoundExcep
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -17,6 +18,10 @@ public class PostService {
     public Post findById(String id) {
         Optional<Post> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+    }
+
+    public List<Post> findByTitle(String text) {
+        return repo.findByTitleContainingIgnoreCase(text);
     }
 
 }
