960c87a6dddd3eee679b7d2411dfff573ab83e8f
nicolasgandrade <nicolasguerrero72@gmail.com>
2021-12-14 11:08:55 -0300

    feat: add AuthorDTO

--- src/main/java/com/nicolasgandrade/restfulmongodb/config/Instantiation.java
+++ src/main/java/com/nicolasgandrade/restfulmongodb/config/Instantiation.java
@@ -2,6 +2,7 @@ package com.nicolasgandrade.restfulmongodb.config;
 
 import com.nicolasgandrade.restfulmongodb.domain.Post;
 import com.nicolasgandrade.restfulmongodb.domain.User;
+import com.nicolasgandrade.restfulmongodb.dto.AuthorDTO;
 import com.nicolasgandrade.restfulmongodb.repository.PostRepository;
 import com.nicolasgandrade.restfulmongodb.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,10 +36,12 @@ public class Instantiation implements CommandLineRunner {
         User rafaela = new User(null, "Rafaela Gomes", "rafaela@gmail.com");
         User gustavo = new User(null, "Gustavo Vieira", "gustavo@gmail.com");
 
-        Post post1 = new Post(null, sdf.parse("12/12/2012"), "Hoje é um dia especial", "Hoje é dia 12, do mês 12, no ano de 2012!", maria);
-        Post post2 = new Post(null, sdf.parse("01/01/2013"), "Feliz ano novo!", "Feliz ano novo, galera! Um ano muito próspero para vocês!", rafaela);
-
+        //Salva primeiro os usuários para depois serem passados ao AuthorDTO
         userRepository.saveAll(Arrays.asList(maria, rafaela, gustavo));
+
+        Post post1 = new Post(null, sdf.parse("12/12/2012"), "Hoje é um dia especial", "Hoje é dia 12, do mês 12, no ano de 2012!", new AuthorDTO(maria));
+        Post post2 = new Post(null, sdf.parse("01/01/2013"), "Feliz ano novo!", "Feliz ano novo, galera! Um ano muito próspero para vocês!", new AuthorDTO(rafaela));
+
         postRepository.saveAll(Arrays.asList(post1, post2));
 
     }
