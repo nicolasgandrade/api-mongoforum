a0c713ba8d856b843979d23fed200a277c836d0a
nicolasgandrade <nicolasguerrero72@gmail.com>
2021-12-14 12:30:42 -0300

    feat: add findById method to posts

---
+++ src/main/java/com/nicolasgandrade/restfulmongodb/services/PostService.java
@@ -0,0 +1,22 @@
+package com.nicolasgandrade.restfulmongodb.services;
+
+import com.nicolasgandrade.restfulmongodb.domain.Post;
+import com.nicolasgandrade.restfulmongodb.repository.PostRepository;
+import com.nicolasgandrade.restfulmongodb.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repo;
+
+    public Post findById(String id) {
+        Optional<Post> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+    }
+
+}
